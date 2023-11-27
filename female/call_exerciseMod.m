ca2d14238d76957c71b6b228b8d7c174b64a8897
Stéphanie Abo <62673310+Stephanie-Abo@users.noreply.github.com>
2023-11-27 13:51:59 -0500

    Add files via upload

---
+++ female/call_exerciseMod.m
@@ -0,0 +1,306 @@
+function [T,Y, dydt, bflow,MR,P,U,F,uprel,epinephrine,c0] = call_exerciseMod()
+
+%% CALL FUNCTION
+% ---------------------------------------------------------------------- 
+% Main simulation code for the whole body model during exercise
+% Stéphanie Abo, last updated 11/27/2023
+%----------------------------------------------------------------------
+
+% close all;
+% clear;
+% clc;
+
+%NEQ = Number of Equations 
+    neq = 156;
+%simulation time
+    t0 = 0;
+    tstart=10; % start time of exercise
+    tend=70;   % end of exercise 
+    tspan = t0:0.01:tend+60; % total simulation time
+
+%SET INITIAL CONDITIONS
+%Initial tissue concentrations for the substrates.
+%There is one set of initial conditions for each tissue compartment. 
+% The order of substrates are as follows: 
+% S1 = Glucose (GLC)
+% S2 = Pyruvate (PYR)
+% S3 = Lactate (LAC)
+% S4 = Alanine (ALA)
+% S5 = Glycerol (GLR)
+% S6 = Free Fatty Acid (FFA)
+% S7 = Triglyceride (TG)
+% S8 = O2
+% S9 = CO2
+% S10 = Glucose-6-Phosphate (G6P)
+% S11 = Glycogen (GLY)
+% S12 = Glyceraldehyde 3-Phosphate (GAP)
+% S13 = Glycerol-3oPhosphate (GRP)
+% S14 = Acetyl CoA (ACoA)
+% S15 = CoA
+% S16 = NAD+
+% S17 = NADH
+% S18 = ATP
+% S19 = ADP
+% S20 = Pi
+% S21 = Phospho Creatine(PCR) 
+% S22 = Creatine(CR)
+
+%Initial arterial concentrations of substrates
+    Ca0(1) = 4.91;   %GLC 
+    Ca0(2) = 0.068;  %PYR 
+    Ca0(3) = 0.7;    %LAC 
+    Ca0(4) = 0.192;  %ALA 
+    Ca0(5) = 0.07;   %GLR 
+    Ca0(6) = 0.76;   %FFA 
+    Ca0(7) = 0.93;   %TG 
+    Ca0(8) = 8.0;    %O2 (assumed constant)
+    Ca0(9) = 21.7;   %CO2 (assumed constant)
+    Ca0(10:22) = 0.0;
+
+% Initial conditions for the brain (B) compartment - y1(x)
+    y10(1) = 1.12;     %GLC     
+    y10(2) = 0.15;     %PYR
+    y10(3) = 1.45;     %LAC
+    y10(4) = 0.0;      %ALA
+    y10(5) = 0.0;      %GLR
+    y10(6) = 0.0;      %FFA
+    y10(7) = 0.0;      %TGL
+    y10(8) = 0.027;    %O2
+    y10(9) = 15.43;    %CO2
+    y10(10) = 0.16;    %G6P
+    y10(11) = 2.0;     %GLY
+    y10(12) = 0.15;    %GAP
+    y10(13) = 0.0;     %GRP
+    y10(14) = 0.0068;  %ACoA
+    y10(15) = 0.06;    %CoA
+    y10(16) = 0.064;   %NAD+
+    y10(17) = 0.026;   %NADH
+    y10(18) = 2.45;    %ATP
+    y10(19) = 0.54;    %ADP
+    y10(20) = 2.40;    %Pi
+    y10(21) = 4.60;    %PCR
+    y10(22) = 5.60;    %CR
+
+%Initial conditions for the heart (H) compartment - y2(x)
+    y20(1) = 1.0;      %GLC
+    y20(2) = 0.2;      %PYR
+    y20(3) = 3.88;     %LAC
+    y20(4) = 0.0;      %ALA
+    y20(5) = 0.015;    %GLR
+    y20(6) = 0.021;    %FFA
+    y20(7) = 3.12;     %TGL
+    y20(8) = 0.96;     %O2
+    y20(9) = 20.0;     %CO2
+    y20(10) = 0.17;    %G6P
+    y20(11) = 33.0;    %GLY
+    y20(12) = 0.01;    %GAP
+    y20(13) = 0.29;    %GRP
+    y20(14) = 0.0012;  %AcoA
+    y20(15) = 0.012;   %CoA
+    y20(16) = 0.40;    %NAD+
+    y20(17) = 0.045;   %NADH
+    y20(18) = 3.4;     %ATP
+    y20(19) = 0.02;    %ADP
+    y20(20) = 1.66;    %Pi
+    y20(21) = 8.3;     %PCR
+    y20(22) = 3.5;     %CR
+
+%Initial conditions for the skeletal muscle (M) compartment - y3(x)
+    y30(1) = 0.48;     %GLC
+    y30(2) = 0.048;    %PYR
+    y30(3) = 1.44;     %LAC
+    y30(4) = 1.3;      %ALA 
+    y30(5) = 0.064;    %GLR 
+    y30(6) = 0.53;     %FFA 
+    y30(7) = 14.8*1.28; %TGL  % 28\% higher in female; see https://doi.org/10.1152/ajpregu.00510.2007
+    y30(8) = 0.49;     %O2 
+    y30(9) = 15.43;    %CO2 
+    y30(10) = 0.24;    %G6P 
+    y30(11) = 95.0;    %GLY 
+    y30(12) = 0.08;    %GAP 
+    y30(13) = 0.15;    %GRP 
+    y30(14) = 0.0022;  %ACoA 
+    y30(15) = 0.018;   %CoA 
+    y30(16) = 0.45;    %NAD+ 
+    y30(17) = 0.05;    %NADH 
+    y30(18) = 6.15;    %ATP 
+    y30(19) = 0.02;    %ADP 
+    y30(20) = 2.70;    %Pi 
+    y30(21) = 20.1;    %PCR 
+    y30(22) = 10.45;   %CR
+
+%Initial conditions for the GI (G) compartment - y4(x)
+    y40(1) = 1.0;     %GLC 
+    y40(2) = 0.2;     %PYR 
+    y40(3) = 3.88;    %LAC 
+    y40(4) = 0.0;     %ALA 
+    y40(5) = 0.015;   %GLR 
+    y40(6) = 0.021;   %FFA 
+    y40(7) = 450.0;   %TGL 
+    y40(8) = 0.49;    %O2 
+    y40(9) = 15.43;   %CO2 
+    y40(10) = 0.17;   %G6P 
+    y40(11) = 33.0;   %GLY 
+    y40(12) = 0.01;   %GAP 
+    y40(13) = 0.29;   %GRP 
+    y40(14) = 0.0012; %ACoA 
+    y40(15) = 0.012;  %CoA 
+    y40(16) = 0.40;   %NAD+ 
+    y40(17) = 0.045;  %NADH 
+    y40(18) = 3.4;    %ATP 
+    y40(19) = 0.02;   %ADP 
+    y40(20) = 1.66;   %Pi 
+    y40(21) = 8.3;    %PCR 
+    y40(22) = 3.5;    %CR
+
+%Initial conditions for the liver (L) compartment - y5(x)
+    y50(1) = 8.0;     %GLC
+    y50(2) = 0.37;    %PYR
+    y50(3) = 0.82;    %LAC
+    y50(4) = 0.23;    %ALA
+    y50(5) = 0.07;    %GLR
+    y50(6) = 0.57;    %FFA
+    y50(7) = 2.93;    %TGL
+    y50(8) = 0.027;   %O2
+    y50(9) = 15.43;   %C02
+    y50(10) = 0.2;    %G6P
+    y50(11) = 417;    %GLY
+    y50(12) = 0.11;   %GAP
+    y50(13) = 0.24;   %GRP
+    y50(14) = 0.035;  %ACoA 
+    y50(15) = 0.14;   %CoA 
+    y50(16) = 0.45;   %NAD+ 
+    y50(17) = 0.05;   %NADH 
+    y50(18) = 2.74;   %ATP 
+    y50(19) = 1.22;   %ADP 
+    y50(20) = 4.60;   %Pi 
+    y50(21) = 0.0;    %PCR 
+    y50(22) = 0.0;    %CR
+
+%Initial conditions for the adipose tissue (A) compartment - y6(x)
+    y60(1) = 2.54;   %GLC 
+    y60(2) = 0.37;   %PYR 
+    y60(3) = 0.82;   %LAC 
+    y60(4) = 0.0;    %ALA 
+    y60(5) = 0.22;   %GLR 
+    y60(6) = 0.57;   %FA 
+    y60(7) = 990.0;  %TGL 
+    y60(8) = 0.027;  %O2 
+    y60(9) = 15.43;  %CO2 
+    y60(10) = 0.2;   %G6P 
+    y60(11) = 0.0;   %GLY 
+    y60(12) = 0.11;  %GAP 
+    y60(13) = 0.24;  %GRP 
+    y60(14) = 0.035; %ACoA 
+    y60(15) = 0.14;  %CoA 
+    y60(16) = 0.45;  %NAD+ 
+    y60(17) = 0.05;  %NADH 
+    y60(18) = 2.74;  %ATP 
+    y60(19) = 1.22;  %ADP 
+    y60(20) = 4.60;  %Pi 
+    y60(21) = 0.0;   %PCR 
+    y60(22) = 0.0;   %CR
+
+%Gather initial conditions   
+    y0(1:22)    = Ca0(1:22);
+    y0(23:44)   = y10(1:22); 
+    y0(45:66)   = y20(1:22); 
+    y0(67:88)   = y30(1:22); 
+    y0(89:110)  = y40(1:22); 
+    y0(111:132) = y50(1:22); 
+    y0(133:154) = y60(1:22); 
+    y0(155) = 39; %Glucagon vs ------> male value = 27
+    y0(156) = 60; %Insulin  vs ------> male value = 45
+    ins0 = y0(156);
+    glu0 = y0(155);
+
+%Set work rate (WR)
+    WR = 150;
+
+% model parameters
+c0=[0.75  7.659	-0.2144	0.1000	2.850	0.1330	0.336	0.02	0.007	2.674	0.3000	0.1	12.30	1440000	6.055	1210000	0.53	1000000	93.15	1210000	0.0143	1705	2.2	1000000	8	912.1	2	198000	4	0.08	2	1440000	0.2	0.4	0.014	0.4	0.04	0.2	1.285	0.10	1.453	0.06	3	0.02	1	810000	2.2	  0.85	1.2 0.1];
+c0 = round(c0,4,'significant');
+
+%Set ODEsolver Parameter Values
+tic;
+    options = odeset('RelTol',1e-6,'AbsTol',1e-8,'Stats','on','NonNegative',1:neq,'Events',@myEventsFcn); 
+    lastwarn('');
+    [T,Y,te,ye,ie] = ode15s(@(t,y) exerciseMod(t,y,WR,ins0,glu0,c0,tstart,tend),tspan,y0,options);
+    [msg,msgID] = lastwarn;
+    if ~isempty(msg)
+            T=tspan';
+            Y=zeros(length(T), length(y0));
+    end
+toc;
+
+%% Save the data
+% Y1(:,1:22)=Y(:,23:44);   %brain
+% Y2(:,1:22)=Y(:,45:66);   %heart
+% Y3(:,1:22)=Y(:,67:88);   %muscle
+% Y4(:,1:22)=Y(:,89:110);  %GI
+% Y5(:,1:22)=Y(:,111:132); %liver
+% Y6(:,1:22)=Y(:,133:154); %adipose
+% arterial = Y(:,1:22);    %arterial substrates
+% hormone = Y(:,155:156);  %glucagon and insulin
+
+[dydt,bflow,MR,P,U,uprel,F,epi] = cellfun(@(t,y) exerciseMod(t,y,WR,ins0,glu0,c0,tstart,tend),num2cell(T),num2cell(Y,2),'uni',0);
+
+bflow = cell2mat(bflow); %blood flow
+MR = cell2mat(MR);       %metabolic rate of ATP -> ADP
+epinephrine = cell2mat(epi);
+
+% %preallocation
+% P1=zeros(size(T,1),22); P2=P1; P3=P1; P4=P1; P5=P1; P6=P1;
+% U1=zeros(size(T,1),22); U2=U1; U3=U1; U4=U1; U5=U1; U6=U1;
+% F1=zeros(size(T,1),25); F2=F1; F3=F1; F4=F1; F5=F1; F6=F1;
+% UR1=zeros(size(T,1),9); UR2=UR1; UR3=UR1; UR4=UR1; UR5=UR1; UR6=UR1; UR7=UR1;
+% 
+% deriv=zeros(size(Y));
+% for j=1:size(T,1)
+%     %production
+%     P1(j,:) = P{j}(1,:);
+%     P2(j,:) = P{j}(2,:);
+%     P3(j,:) = P{j}(3,:);
+%     P4(j,:) = P{j}(4,:);
+%     P5(j,:) = P{j}(5,:);
+%     P6(j,:) = P{j}(6,:);
+%     
+%     %utilization
+%     U1(j,:) = U{j}(1,:);
+%     U2(j,:) = U{j}(2,:);
+%     U3(j,:) = U{j}(3,:);
+%     U4(j,:) = U{j}(4,:);
+%     U5(j,:) = U{j}(5,:);
+%     U6(j,:) = U{j}(6,:);
+%     
+%     %uptake-release rates
+%     UR1(j,:) = uprel{j}(1,1:9);
+%     UR2(j,:) = uprel{j}(2,1:9);
+%     UR3(j,:) = uprel{j}(3,1:9);
+%     UR4(j,:) = uprel{j}(4,1:9);
+%     UR5(j,:) = uprel{j}(5,1:9);
+%     UR6(j,:) = uprel{j}(6,1:9);
+%     UR7(j,:) = uprel{j}(7,1:9); % other tissues
+%     
+%     %metabolic fluxes
+%     F1(j,:) = F{j}(1,:);
+%     F2(j,:) = F{j}(2,:);
+%     F3(j,:) = F{j}(3,:);
+%     F4(j,:) = F{j}(4,:);
+%     F5(j,:) = F{j}(5,:);
+%     F6(j,:) = F{j}(6,:);
+% 
+%     %miscellaneous
+%     deriv(j,1:156) = dydt{j};
+% end    
+
+% % control output for sanity check
+% timestep = diff(T);
+% maxstep  = max(diff(T));
+% minstep  = min(diff(T));
+% maxdydt  = max(deriv,[],'all');
+
+% save('female.mat','arterial','Y1','Y2','Y3','Y4','Y5','Y6','Y','uprel',...
+%     'P1','P2','P3','P4','P5','P6','U1','U2','U3','U4','U5','U6',...
+%     'UR1','UR2','UR3','UR4','UR5','UR6','UR7','F1','F2','F3','F4','F5','F6','hormone','T','deriv',...
+%      'MR','bflow','epinephrine','c0','y0','t0','tend','WR','ins0','glu0') 
---
+++ female/validationFemale.m
@@ -0,0 +1,329 @@
+ %% Comparing model simulation results to data
+close all;
+clear;
+clc;
+
+[T,Y,dydt,bflow,MR,P,U,F,uprel,epinephrine,c0] = call_exerciseMod();
+
+%% Hirsch1991
+% Time
+t1 = [0 10 20 30 40 50 60]';
+% Arterial insulin concentration (pM) 
+ins = [59 46 37 35 32 29 29]';
+ins_sd = [8 8 4 7 3 4 4]';
+% Relative arterial FFA concentration (unitless)
+ffa = [0.32 0.26 0.26 0.30 0.33 0.37 0.41]/0.32;
+ffa_sd = [0.04 0.03 0.03 0.03 0.04 0.04 0.06]/0.32;
+
+%% Horton et al. 2006
+% Arterial glucagon concentration (pM)
+% fractional change: starting point calculated by (HortonF/HortonM) * HirschM
+% t = [-15	30	60	75	90	120	150	210	280];
+% glu = [39.55	42.27	42.46	45.30	49.24	47.39	41.30	41.68	41.91]; %full data
+% glu_sd = [2.01	3.57	2.34	2.68	2.90	2.90	2.57	2.35	1.79]; % full data
+t2 = [-15 30 60 75 90]'; 
+glu = [39.55	42.27	42.46	45.30	49.24]';
+glu_sd = [2.01	3.57	2.34	2.68	2.90]';
+
+% Glucagon-Insulin ratio (unitless)
+t3 = [30 60]';
+gir = glu([2,3])./ins([4,7]);
+    % Standard Deviation (SD) in this case is the difference between the 
+    % highest value that the GIR can take and the average GIR. 
+    % The GIR is highest when glucagon is at a maximum and insulin is at a 
+    % minimum -> (glu+glu_sd)./(ins-ins_sd) - gir
+gir_sd = (glu([2,3])+glu_sd([2,3]))./(ins([4,7])-ins_sd([4,7])) - gir;
+
+t4 = [0 15 30 45 60]';
+% Whole body glucose production (mmol/min)
+Ra = [0.74 1.25 1.19 1.17 1.19]';
+Ra_sd =[0.06 0.28 0.28 0.28 0.28]';
+% Whole body glucose utilization (mmol/min)
+Rd = [0.77 1.28 1.25 1.19 1.25]';
+Rd_sd =[0.06 0.28 0.28 0.28 0.28]';
+% whole body glucoe balance Ra - Rd (mmol/min)
+bal = Ra - Rd;
+% SD in this case is the difference between the highest value that the
+    % whole body glucose balance can take and the average glucose balance. 
+    % The balance is highest when production is at its maximum while 
+    % utilization is at its minimum -> (Ra+Ra_sd)-(Rd-Rd_sd)  
+bal_sd = ((Ra+Ra_sd) - (Rd-Rd_sd)) - bal;
+
+t5 = [0 10 20 30 45 60]';
+% Relative arterial lactate concentration (unitless)
+lac = [0.4 1.2 1.0 0.75 0.7 0.65]'/0.4;
+lac_sd = [0.10 0.40 0.40 0.20 0.20 0.10]'/0.4;
+
+%% vanHall2002
+% Time of exercise
+t6 = [20 40 60];
+% Relative arterial glycerol concentration (unitless)
+glr = [0.166 0.225 0.251]/0.075; % 0.075 is the measurement at t=-10. 
+glr_sd = [0.0165 0.0174 0.0202]/0.075;
+
+%% Horton et al. 2006
+% Time of exercise
+t7 = [-15 0 10 20 30 45 60 75 90 110 120 150 210 270];
+% Arterial epinephrine concentration (pM)
+% epi_exp_pg_pmol = [27 80 73 60 68 70 90 105 24 25 27 25 27]; % concentration in pg
+epi_exp = [147 147 437 398 328 371 382 491 573 131 136 147 136 147]; % pmol
+epi_exp_sd = [16 16 82 109 82 109 82 218 218 16 16 16 16 16];
+
+%% Horton et al. 2002 
+% Time of exercise
+t8 = [-15 0 10 20 30 45 60 75 90];
+% Arterial epinephrine concentration (pM)
+epi_exp_pg_pmol_2 = [23 23 67 70 55 69 71 74 90];
+epi_exp_2 = [126 126 366 382 300 377 388 404 491];
+epi_exp_sd_2 = [20 20 55 136 109 66 109 109 136];
+
+%% Campbell et al. 2001
+t3camp = [0 30 60]';
+% arterial glucose concentration (mM)
+glc = [4.75 4.9 4.65]';
+glc_sd = [0.2 0.4 0.3]';
+
+%% get data
+Y1(:,1:22)=Y(:,23:44);   %brain
+Y2(:,1:22)=Y(:,45:66);   %heart
+Y3(:,1:22)=Y(:,67:88);   %muscle
+Y4(:,1:22)=Y(:,89:110);  %GI
+Y5(:,1:22)=Y(:,111:132); %liver
+Y6(:,1:22)=Y(:,133:154); %adipose
+arterial = Y(:,1:22);    %arterial substrates
+hormone = Y(:,155:156);  %glucagon and insulin
+%preallocation
+P1=zeros(size(T,1),22); P2=P1; P3=P1; P4=P1; P5=P1; P6=P1;
+U1=zeros(size(T,1),22); U2=U1; U3=U1; U4=U1; U5=U1; U6=U1;
+F1=zeros(size(T,1),25); F2=F1; F3=F1; F4=F1; F5=F1; F6=F1;
+UR1=zeros(size(T,1),9); UR2=UR1; UR3=UR1; UR4=UR1; UR5=UR1; UR6=UR1; UR7=UR1;
+% epinephrine = zeros(size(T,1),1);
+
+deriv=zeros(size(Y));
+for j=1:size(T,1)
+    %production
+    P1(j,:) = P{j}(1,:);
+    P2(j,:) = P{j}(2,:);
+    P3(j,:) = P{j}(3,:);
+    P4(j,:) = P{j}(4,:);
+    P5(j,:) = P{j}(5,:);
+    P6(j,:) = P{j}(6,:);
+    
+    %utilization
+    U1(j,:) = U{j}(1,:);
+    U2(j,:) = U{j}(2,:);
+    U3(j,:) = U{j}(3,:);
+    U4(j,:) = U{j}(4,:);
+    U5(j,:) = U{j}(5,:);
+    U6(j,:) = U{j}(6,:);
+    
+    %uptake-release rates
+    UR1(j,:) = uprel{j}(1,1:9);
+    UR2(j,:) = uprel{j}(2,1:9);
+    UR3(j,:) = uprel{j}(3,1:9);
+    UR4(j,:) = uprel{j}(4,1:9);
+    UR5(j,:) = uprel{j}(5,1:9);
+    UR6(j,:) = uprel{j}(6,1:9);
+    UR7(j,:) = uprel{j}(7,1:9); % other tissues
+    
+    %metabolic fluxes
+    F1(j,:) = F{j}(1,:);
+    F2(j,:) = F{j}(2,:);
+    F3(j,:) = F{j}(3,:);
+    F4(j,:) = F{j}(4,:);
+    F5(j,:) = F{j}(5,:);
+    F6(j,:) = F{j}(6,:);
+
+    %miscellaneous
+    deriv(j,1:156) = dydt{j};
+end
+
+%% Plotting
+Time = T-10;
+
+%-----------------------------------------------------------------
+figure(1)
+tiledlayout(1,2)
+% Tile 1
+nexttile
+plot(Time,hormone(:,2),'--k','linewidth',2) % Insulin
+hold on
+plot(Time,hormone(:,1),'-k','linewidth',2)  %Glucagon
+errorbar(t1,ins,ins_sd,'s','MarkerSize',10,...
+    'MarkerEdgeColor','k','MarkerFaceColor','k','Color','k') % Insulin
+errorbar(t2,glu,glu_sd,'^','MarkerSize',10,...
+    'MarkerEdgeColor','k','MarkerFaceColor','k','Color','k') %Glucagon
+hold off
+xlim([-10,120])
+xticks([-10 0 10 20 30 40 50 60 70 80 90 100 110 120 130])
+xticklabels({-10','0','10','20','30','40','50','60', '70', '80', '90', '100', '110', '120' , ''})
+ylim([0,70])
+xlabel('Time, min','FontWeight','B')
+ylabel('Concentration, pM','FontWeight','B')
+legend('Insulin','Glucagon','Insulin (experiment)','Glucagon (experiment)')
+set(gca,'FontSize',15)
+% Tile 2
+nexttile
+plot(Time,hormone(:,1)./hormone(:,2),'-k','linewidth',2) % GIR
+hold on
+errorbar(t3,gir,gir_sd,'s','MarkerSize',10,...
+    'MarkerEdgeColor','k','MarkerFaceColor','k','Color','k') % GIR
+hold off
+xlim([-10,120])
+xticks([-10 0 10 20 30 40 50 60 70 80 90 100 110 120 130])
+xticklabels({-10','0','10','20','30','40','50','60', '70', '80', '90', '100', '110', '120' , ''})
+ylim([0,2])
+xlabel('Time, min','FontWeight','B')
+ylabel('Glucagon-Insulin ratio','FontWeight','B')
+legend('GIR','GIR (experiment)')
+set(gca,'FontSize',15)
+
+figure(2)
+tiledlayout(1,2)
+% Tile 1
+nexttile
+utilization = UR1+UR2+UR3+UR4+UR6+UR7;
+production = -UR5; %UR5 is negative, thus we have production = (-)release rate.
+plot(Time, production(:,1), '-k', 'linewidth', 2)
+hold on
+errorbar(t4,Ra,Ra_sd,'s','MarkerSize',10,...
+    'MarkerEdgeColor','k','MarkerFaceColor','k','Color','k') % whole body GLC production
+hold off
+xlim([-10,120])
+xticks([-10 0 10 20 30 40 50 60 70 80 90 100 110 120 130])
+xticklabels({-10','0','10','20','30','40','50','60', '70', '80', '90', '100', '110', '120' , ''})
+ylim([0,3])
+xlabel('Time, min','FontWeight','B')
+ylabel('Whole body glucose production','FontWeight','B')
+legend('Simulation','Experiment')
+set(gca,'FontSize',15)
+% Tile 2
+nexttile
+balance = production - utilization; 
+plot(Time, balance(:,1), '-k', 'linewidth', 2)
+hold on
+errorbar(t4,bal,bal_sd,'s','MarkerSize',10,...
+    'MarkerEdgeColor','k','MarkerFaceColor','k','Color','k') % whole body GLC balance
+hold off
+xlim([-10,120])
+xticks([-10 0 10 20 30 40 50 60 70 80 90 100 110 120 130])
+xticklabels({-10','0','10','20','30','40','50','60', '70', '80', '90', '100', '110', '120' , ''})
+ylim([-2,2])
+xlabel('Time, min','FontWeight','B')
+ylabel('Whole body glucose balance','FontWeight','B')
+legend('Simulation','Experiment')
+set(gca,'FontSize',15)
+
+figure(3)
+tiledlayout(1,2)
+% Tile 1
+nexttile
+plot(Time, arterial(:,1), '-k', 'linewidth', 2)
+hold on
+errorbar(t3camp,glc,glc_sd,'s','MarkerSize',10,...
+    'MarkerEdgeColor','k','MarkerFaceColor','k','Color','k') % glucose
+hold off
+xlim([-10,120])
+xticks([-10 0 10 20 30 40 50 60 70 80 90 100 110 120 130])
+xticklabels({-10','0','10','20','30','40','50','60', '70', '80', '90', '100', '110', '120' , ''})
+ylim([1,6])
+xlabel('Time, min','FontWeight','B')
+ylabel('GLC conc. (mM)','FontWeight','B')
+legend('Simulation','Experiment')
+set(gca,'FontSize',15)
+% Tile 2
+nexttile
+plot(Time, arterial(:,6)./arterial(1,6), '-k', 'linewidth', 2)
+hold on
+errorbar(t1,ffa,ffa_sd,'s','MarkerSize',10,...
+    'MarkerEdgeColor','k','MarkerFaceColor','k','Color','k') % relative FFA
+hold off
+xlim([-10,120])
+xticks([-10 0 10 20 30 40 50 60 70 80 90 100 110 120 130])
+xticklabels({-10','0','10','20','30','40','50','60', '70', '80', '90', '100', '110', '120' , ''})
+ylim([0,1.5])
+xlabel('Time, min','FontWeight','B')
+ylabel('Rel. art. FFA conc. (unitless)','FontWeight','B')
+legend('Simulation','Experiment')
+set(gca,'FontSize',15)
+%-----------------------------------------------------------------
+
+figure(4)
+tiledlayout(1,2)
+% Tile 1
+nexttile
+plot(Time, arterial(:,5)./arterial(1,5), '-k', 'linewidth', 2)
+hold on
+errorbar(t6,glr,glr_sd,'s','MarkerSize',10,...
+    'MarkerEdgeColor','k','MarkerFaceColor','k','Color','k') % relative GLR
+hold off
+xlim([-10,120])
+xticks([-10 0 10 20 30 40 50 60 70 80 90 100 110 120 130])
+xticklabels({-10','0','10','20','30','40','50','60', '70', '80', '90', '100', '110', '120' , ''})
+xlabel('Time, min','FontWeight','B')
+ylabel('Rel. art. GLR conc. (unitless)','FontWeight','B')
+legend('Simulation','Experiment')
+set(gca,'FontSize',15)
+% Tile 2
+nexttile
+plot(Time, arterial(:,3)./arterial(1,3), '-k', 'linewidth', 2)
+hold on
+errorbar(t5,lac,lac_sd,'s','MarkerSize',10,...
+    'MarkerEdgeColor','k','MarkerFaceColor','k','Color','k') % relative LAC
+hold off
+xlim([-10,120])
+xticks([-10 0 10 20 30 40 50 60 70 80 90 100 110 120 130])
+xticklabels({-10','0','10','20','30','40','50','60', '70', '80', '90', '100', '110', '120' , ''})
+xlabel('Time, min','FontWeight','B')
+ylabel('Rel. art. LAC conc. (unitless)','FontWeight','B')
+legend('Simulation','Experiment')
+set(gca,'FontSize',15)
+
+figure(5)
+plot(Time,epinephrine,'-','linewidth',2) % epinephrine
+hold on
+errorbar(t7,epi_exp,epi_exp_sd,'s','MarkerSize',10,...
+    'MarkerEdgeColor','k','MarkerFaceColor','k','Color','k') 
+hold off
+xlim([-10,120])
+xticks([-10 0 10 20 30 40 50 60 70 80 90 100 110 120 130])
+xticklabels({-10','0','10','20','30','40','50','60', '70', '80', '90', '100', '110', '120' , ''})
+ylim([0,700])
+xlabel('Time, min','FontWeight','B')
+ylabel('Concentration of epinephrine, pM','FontWeight','B')
+legend('Epinephrine', 'Epinephrine (experiment)')
+set(gca,'FontSize',15)
+
+%-----------------------------------------------------------------
+figure(8)
+plot(T,UR1(:,8),"LineWidth",2)
+hold on
+plot(T,UR2(:,8),"LineWidth",2)
+plot(T,UR3(:,8),"LineWidth",2)
+plot(T,UR4(:,8),"LineWidth",2)
+plot(T,UR5(:,8),"LineWidth",2)
+plot(T,UR6(:,8),"LineWidth",2)
+hold off
+ylabel('O2 flux')
+xlabel('Time (min)')
+legend('B','H','M','G','L','A')
+set(gca,'FontName','Helvetica','FontSize',14)
+
+figure(9)
+plot(T,UR1(:,9),"LineWidth",2)
+hold on
+plot(T,UR2(:,9),"LineWidth",2)
+plot(T,UR3(:,9),"LineWidth",2)
+plot(T,UR4(:,9),"LineWidth",2)
+plot(T,UR5(:,9),"LineWidth",2)
+plot(T,UR6(:,9),"LineWidth",2)
+hold off
+ylabel('CO2 flux')
+xlabel('Time (min)')
+legend('B','H','M','G','L','A')
+set(gca,'FontName','Helvetica','FontSize',14)
+
+%% checking RQ at steady state
+V = round(22.4*uprel{1,1}(:,8:9),4,'significant');
+WB = sum(V,1);
+RQ0_WB = -WB(2)/WB(1);
