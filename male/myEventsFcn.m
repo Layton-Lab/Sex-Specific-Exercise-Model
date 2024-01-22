f57bd7031dd2503d83aced0e7f1da61b7e28fd08
Stephanie-Abo <62673310+Stephanie-Abo@users.noreply.github.com>
2024-01-22 11:40:14 -0500

    Fixed a bug in the model files

--- male/myEventsFcn.m
+++
@@ -1,5 +0,0 @@
-function [position,isterminal,direction] = myEventsFcn(t,y)
-  position = t-10; % The value that we want to be zero
-  isterminal = 0;  % Do not halt integration 
-  direction = 0;   % The zero can be approached from either direction
-end
